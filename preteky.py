4261dea302f16f87133e096edd48d25eeed1d77f
brunovskyoliver <obrunovsky7@gmail.com>
2024-04-19 19:37:36 +0200

    init

---
+++ preteky.py
@@ -0,0 +1,13 @@
+import tkinter, random
+
+screen_width = 750
+screen_height = 450
+
+t = tkinter.Tk()
+c = tkinter.Canvas(width=screen_width,height=screen_height,bg="white")
+c.pack()
+
+
+
+
+t.mainloop()
